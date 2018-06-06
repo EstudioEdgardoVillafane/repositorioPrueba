e82f4614b2b8ea16cb02357f17e1fa25b3526503
Fluzko <fluzko@gmail.com>
2018-06-06 14:47:06 -0300

    instalado material :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{CoreModule} from './core/core.module';
 import { AppComponent } from './app.component';
-
+import { TestComponent } from './test/test.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule,MatMenuModule, MatToolbarModule, MatIconModule} from '@angular/material';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    CoreModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+MatMenuModule
